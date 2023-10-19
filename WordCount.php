5512169fae6f0c031f6237ea1f6081f751683adf
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-10-19 14:26:59 +0700

    added additional comment on some line

--- WordCount.php
+++ WordCount.php
@@ -6,6 +6,7 @@ class WordCount
     {
         return count(explode(" ", $sentence));
 
+        //return berfungsi untuk mengembalikan hasil perhitungan jumlah kata dari function "countWords"
         //explode pada line 7 berfungsi untuk memecah String menjadi Array
         //contohnya pada kalimat yang saya buat 'My name is thoriq and i like music' fungsi explode akan memecah String menjadi array per kata dengan pemisah (Delimiter) spasi (" ", $sentence)
     }
